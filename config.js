847bcc71b0d121fd3ce9ff8e3008ccfa7663fbfb
anydatne <106884349+anydatne@users.noreply.github.com>
2022-06-05 02:49:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "❤️Love❤️",
+    introTitle: 'Ỏ bạn nữ xinh đẹp❤️',
+    introDesc: `Ỏ có bạn nữ xinh đẹp nhấp vô link kìa❤️
+    bạn ấy thật đẹp❤️`,
+    btnIntro: 'Nhấp❤️',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tặng cậu trái❤️ nè Đạt',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tặng bạn Trái ❤️ siuuuuu to khổng lồ nè.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/AnyNehi.04' 
+}
