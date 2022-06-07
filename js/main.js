fad57ff5d538926df0283a6f7d44062bbd8770e6
anydatne <106884349+anydatne@users.noreply.github.com>
2022-06-07 22:03:04 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,8 +46,6 @@ function firstQuestion() {
 // switch button position
 function switchButton() {
     var audio = new Audio('sound/duck.mp3');
-    var audio = new Audio('sound/sound.mp3');
-    audio.play();
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
